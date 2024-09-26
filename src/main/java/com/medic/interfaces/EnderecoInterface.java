88e6b8d8fa6abefbf8fae3bff4cd86cadfba4865
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-09-26 01:09:45 -0300

    crud endereço

--- src/main/java/com/medic/interfaces/EnderecoInterface.java
+++ src/main/java/com/medic/interfaces/EnderecoInterface.java
@@ -1,5 +1,14 @@
 package com.medic.interfaces;
 
-public interface EnderecoInterface {
+import java.util.List;
 
+import com.medic.model.Endereco;
+
+public interface EnderecoInterface {
+    public void inserirEndereco(Endereco endereco);
+    public Endereco consultarEndereco(int idEndereco);
+    public void editarEndereco(Endereco endereco);
+    public void excluirEndereco(int idEndereco);
+    public List<Endereco> listarEndereco();
+    
 }
--- src/main/java/com/medic/tests/Teste.java
+++ src/main/java/com/medic/tests/Teste.java
@@ -1,18 +1,21 @@
 package com.medic.tests;
 
+import com.medic.model.Endereco;
 import com.medic.model.Especialidade;
 import com.medic.service.ConnectionFactory;
 import com.medic.interfaces.EspecialidadeInterface;
 
 import java.util.List;
 
+import com.medic.dao.EnderecoDAO;
 import com.medic.dao.EspecialidadeDAO;
 /*
+ * Endereco: 
  * Inserir [ok]
  * consultar [ok]
  * excluir [ok]
  * listar [ok]
- * editar [aparentemente ok]
+ * editar [ok]
  * 
  * */
 
@@ -25,6 +28,42 @@ public class Teste {
         System.out.println("Id: "+consulta.getId());
         System.out.println("nome: "+consulta.getNome());*/
         
+
+         EnderecoDAO enderecoDAO = new EnderecoDAO();
+
+        // Criar e inserir um novo endereço
+        /*Endereco enderecoInserido = new Endereco(0, "Rua A", "123", "Apto 101", "Centro", "CidadeX", "UF", "12345-678");
+        enderecoDAO.inserirEndereco(enderecoInserido);
+        System.out.println("Endereço inserido.");*/
+
+        // Consultar o endereço inserido (assumindo que o ID é 1 para exemplo)
+        Endereco enderecoConsultado = enderecoDAO.consultarEndereco(1);
+        //if (enderecoConsultado != null) {
+           // System.out.println("Endereço consultado: " + enderecoConsultado);
+       // } else {
+       //     System.out.println("Endereço não encontrado.");
+        //}
+
+        // Editar o endereço consultado
+       // if (enderecoConsultado != null) {
+           // enderecoConsultado.setLogradouro("Rua B");
+           // enderecoConsultado.setNumero("456");
+           // enderecoDAO.editarEndereco(enderecoConsultado);
+           // System.out.println("Endereço editado.");
+       //}
+
+        // Listar todos os endereços
+        //List<Endereco> listaEnderecos = enderecoDAO.listarEndereco();
+        //System.out.println("Lista de endereços:");
+        //for (Endereco endereco : listaEnderecos) {
+           // System.out.println(endereco);
+        //}
+
+        // Excluir o endereço
+        //if (enderecoConsultado != null) {
+            //enderecoDAO.excluirEndereco(enderecoConsultado.getIdEndereco());
+            //System.out.println("Endereço excluído.");
+        //}
         
         
         			
