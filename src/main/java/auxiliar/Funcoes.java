415d651f1fe84797352b0c5743a6cda1483877b0
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-09-30 14:38:14 -0300

    Classe função adicionada

---
+++ src/main/java/auxiliar/Funcoes.java
@@ -0,0 +1,22 @@
+package auxiliar;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Funcoes {
+	
+	public Date converterDateSQL(String dataString) {
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		java.util.Date dataUtil;
+		
+		try {
+			dataUtil = formato.parse(dataString);
+			return new java.sql.Date(dataUtil.getTime());
+		}catch (ParseException e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+}
