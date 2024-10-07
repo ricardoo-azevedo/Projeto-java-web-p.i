16af20e47abd0913227b008754ead8d80c31242b
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-10-07 16:32:28 -0300

    crud funcionario (depois é formulario T-T)

---
+++ src/main/java/com/medic/model/Funcionario.java
@@ -0,0 +1,41 @@
+package com.medic.model;
+
+public class Funcionario{
+
+    private int id;
+    private String nome;
+    private String senha;
+    
+    public Funcionario(){
+        
+    }
+    public Funcionario(int id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return"[Id: "+this.id+" | Nome: "+this.nome+" | Senha: "+this.senha+"]";
+    }
+}
